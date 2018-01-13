8e65e86553e30262f02b9166a354f51ed840a952
cks1001652 <ycwang21@gmail.com>
2018-01-13 17:09:37 -0500

    update the algo using the updated algo for plain fibonacci

--- Week2/introduction_starter_files/fibonacci_last_digit/fibonacci_last_digit.py
+++ Week2/introduction_starter_files/fibonacci_last_digit/fibonacci_last_digit.py
@@ -1,17 +1,27 @@
 # Uses python3
 import sys
 
+# def get_fibonacci_last_digit_naive(n):
+#     if n <= 1:
+#         return n
+
+#     previous = 0
+#     current  = 1
+
+#     for _ in range(n - 1):
+#         previous, current = current, previous + current
+
+#     return current % 10
+
+
 def get_fibonacci_last_digit_naive(n):
     if n <= 1:
         return n
 
-    previous = 0
-    current  = 1
+    return int(1/np.sqrt(5)*(((1+np.sqrt(5))/2)**n - ((1-np.sqrt(5))/2)**n)) %10
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
 
-    return current % 10
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
