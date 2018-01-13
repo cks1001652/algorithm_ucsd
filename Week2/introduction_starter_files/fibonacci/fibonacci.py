d06eeb2d2e12e814c0384c0dfa629340d4570eac
cks1001652 <ycwang21@gmail.com>
2018-01-13 17:04:05 -0500

    add updates/new algo for fibonachi algo

--- Week2/introduction_starter_files/fibonacci/fibonacci.py
+++ Week2/introduction_starter_files/fibonacci/fibonacci.py
@@ -1,9 +1,20 @@
-# Uses python3
-def calc_fib(n):
-    if (n <= 1):
-        return n
+# python3
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+#simple version
+# def calc_fib(n):
+#     if (n <= 1):
+#         return n
+
+#     return calc_fib(n - 1) + calc_fib(n - 2)
+
+
+import numpy as np
+
+def calc_fib(n):
+    if n<=1:
+        return n
+    return int(1/np.sqrt(5)*(((1+np.sqrt(5))/2)**n - ((1-np.sqrt(5))/2)**n))
+
 
 n = int(input())
 print(calc_fib(n))
